9a21b9f611a99a7e688c1cd7e05183d415ceb47a
AtifSaleem <saleematif75@gmail.com>
2022-09-27 14:35:56 +0500

    Updated Message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Successfully Deployed Nest APP';
+    return 'Successfully Deployed Nest App!';
   }
 }
